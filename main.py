86ffa1a6eb0efd58f79bbf201eae9147a2859fd8
sampawar2577 <sampawar2777@gmail.com>
2023-08-25 19:11:57 +0530

    data ingestion completed

---
+++ main.py
@@ -0,0 +1,15 @@
+from src.cnnClassifier import logger
+from src.cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<\n\n x============x")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
